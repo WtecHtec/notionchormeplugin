0d1c2951a0c2714b9b6e7827b2b82838aea09803
herzshen <herzshen@tencent.com>
2023-12-08 10:28:53 +0800

    init

---
+++ back/main.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+
+const port = 3000
+
+
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
